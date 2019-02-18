2ee9b1d52b7a975fff3882e8e6f8138302178e2e
1asa1 <may5_may5_may5@yahoo.co.jp>
2019-02-18 23:23:07 +0900

    変更 カテゴリーページまで表示されるように

--- mytheme001/category.php
+++ mytheme001/category.php
@@ -1,9 +1,31 @@
 <?php get_header(); ?>
+<section>
+	<?php
+		// 変数categoryに、wordpressのカテゴリーを取得して入れる
+		$category = get_the_category();
+		// 変数cat_name（カテゴリーネーム）に、カテゴリー名を取り出して入れる
+		$cat_name = $category[0] -> cat_name;
+	?>
 
-	<?php if(have_posts()): while(have_posts()):the_post(); ?>
-		<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+	<ul>
+		<?php if(have_posts()): while(have_posts()):the_post(); ?>
+	
+		<li>
+			<span class="red">
+				<!-- 変数cat_nameに変数を入れておいたおかげでカテゴリー名が表示される -->
+				<?php echo $cat_name; ?>
+			</span>
+			<span>
+				<?php echo get_the_date('Y/n/j'); ?>
+			</span>
+			<a href="<?php the_permalink(); ?>">
+				<?php the_title(); ?>
+			</a>
+		</li>
+			
+	</ul>
+		<?php endwhile; endif; ?>
+	
+</section>
 
-	<?php endwhile; endif; ?>
-
-
-<?php get_gooter();?>
+<?php get_footer();?>
