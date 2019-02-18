55b3758e3a58cbd36bf34c3469604aacec20c4c0
1asa1 <may5_may5_may5@yahoo.co.jp>
2019-02-18 23:14:23 +0900

    変更 single.phpに関してコメントをつける

--- mytheme001/single.php
+++ mytheme001/single.php
@@ -6,13 +6,17 @@
 	<section>
 		<h2>
 			<a href="<?php the_permalink(); ?>">
+			<!-- 記事内部のリンク呼び出し -->
 				<?php the_title(); ?>
+				<!-- 記事のタイトル -->
 			</a>
 		</h2>
 		<p><?php echo get_the_date('Y/n/j');?></p>
+		<!-- 投稿日時の表示 -->
 	
 		<div class="contents_area">
 			<?php the_content();?>
+			<!-- コンテンツの本文の呼び出し -->
 		</div><!-- /.contents_area -->
 	
 	
